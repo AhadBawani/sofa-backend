8aa03ec0ffbe99427b414946b1379bb46ec8854c
AhadBawani <ahadbawani123@gmail.com>
2023-12-20 11:46:23 +0530

    20 12 changes

--- config/data.js
+++ config/data.js
@@ -2,73 +2,66 @@ const data = [
     {
         id: 1,
         productImage: '1.jpg',
-        productName: "Ceiling Table",
-        productPrice: "$ 75",
+        productName: "Alpine",
+        productPrice: 33600,
         quantity: 1
     },
     {
         id: 2,
         productImage: '2.jpg',
-        productName: "Table",
-        productPrice: "$ 15",
+        productName: "Streamline",
+        productPrice: 29800,
         quantity: 1
     },
     {
         id: 3,
         productImage: '3.jpg',
-        productName: "Chair",
-        productPrice: "$ 45",
+        productName: "High Back Chair",
+        productPrice: 46500,
         quantity: 1
     },
     {
         id: 4,
         productImage: '4.jpg',
-        productName: "Curtain",
-        productPrice: "$ 175",
+        productName: "Zara",
+        productPrice: 32800,
         quantity: 1
     },
     {
         id: 5,
         productImage: '5.jpg',
-        productName: "Bed",
-        productPrice: "$ 275",
+        productName: "Zest",
+        productPrice: 42600,
         quantity: 1
     },
     {
         id: 6,
         productImage: '6.jpg',
-        productName: "Wood Table",
-        productPrice: "$ 75",
+        productName: "Tesa Golden Ring",
+        productPrice: 29800,
         quantity: 1
     },
     {
         id: 7,
         productImage: '7.jpg',
-        productName: "Sofa",
-        productPrice: "$ 375",
+        productName: "Red Rose",
+        productPrice: 39600,
         quantity: 1
     },
     {
         id: 8,
         productImage: '8.jpg',
-        productName: "3 X 3 Sofe",
-        productPrice: "$ 475",
+        productName: "Casa With Golden Border",
+        productPrice: 48600,
         quantity: 1
     },
     {
         id: 9,
         productImage: '9.jpg',
-        productName: "Ceiling Table",
-        productPrice: "$ 75",
+        productName: "Tulip Full Panel Rose Gold Border",
+        productPrice: 49800,
         quantity: 1
-    },
-    {
-        id: 10,
-        productImage: '10.jpg',
-        productName: "Ceiling Table",
-        productPrice: "$ 75",
-        quantity: 1
-    }
+    },    
 ]
 
 module.exports = data;
